e81c261df5b2cd6ba4fcb3f9b2821a2b38f0dc47
tommyb999 <tom_bale@hotmail.com>
2020-04-13 14:58:42 +0100

    Added WorkItems extension

---
+++ OdeToFood.Web/Controllers/WorkItemsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class WorkItemsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
